6a4939bcbb319e8a7a1f47c45c1e1b635f2ad9c6
Restuta <restuta8@gmail.com>
2014-12-09 15:33:40 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var _ = require('lodash');
+
+//CORS
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+var drafts = [{
+    id: 1,
+    name: 'First listing',
+    minuteRate: 2.99
+}, {
+    id: 2,
+    name: 'Second listing',
+    minuteRate: 5.88
+}];
+
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get('/drafts', function(req, res) {
+    res.send({'drafts':drafts});
+});
+
+app.post('/drafts', function(req, res) {
+
+    var lastDraft = _.max(drafts, function(draft) {
+        return draft.id;
+    })
+
+    var newId = lastDraft.id + 1;
+
+    var newDraft = {
+        id: newId,
+        name: '',
+        minuteRate: 0
+    }
+
+    drafts.push(newDraft);
+    res.status(201);
+
+
+    setTimeout(function(){
+        res.send({'draft': newDraft });    
+    }, 3000)
+    
+})
+
+app.listen(3008);
