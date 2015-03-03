3c074be3e4b00eb9e916eb3a45d63e65691842d7
mramasubramanian <mramasubramanian@ingenio.com>
2015-03-03 14:00:01 -0800

    Adding the profilePicture data to the listing draft structure.

--- db/listing-drafts.js
+++ db/listing-drafts.js
@@ -9,6 +9,42 @@ module.exports = [{
         isPrimary: true,
         formattedNumber: null
     },
+    profilePicture:{
+        id: 8649,
+        primaryVersion: {
+          fileName: "22770061-1849016341.jpg",
+          directoryName: "ad-products.cdn.memberphotos/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.memberphotos/22770061-1849016341.jpg",
+          width: 95,
+          height: 40,
+          alternateText: null
+          },
+        originalVersion: {
+          fileName: "22770061-2087991557.jpg",
+          directoryName: "ad-products.cdn.originalmemberphotos/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.originalmemberphotos/22770061-2087991557.jpg",
+          width: 375,
+          height: 158,
+          alternateText: null
+          },
+        thumbnailVersion: {
+          fileName: "22770061-233085406.jpg",
+          directoryName: "ad-products.cdn.memberthumbs/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.memberthumbs/22770061-233085406.jpg",
+          width: 50,
+          height: 21,
+          alternateText: null
+          },
+        size75Version: {
+          fileName: "22770061-1542256979.jpg",
+          directoryName: "ad-products.cdn.member75x75/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.member75x75/22770061-1542256979.jpg",
+          width: 75,
+          height: 31,
+          alternateText: null
+          },
+        isAssociatedWithService: true
+    },
     categoryId: 642,
     chatEnabled: true,
     minuteRate: 2.99,
@@ -40,11 +76,54 @@ module.exports = [{
     HTMLDescription: '<marquee>This is how advisors have their html description. <b>Its all crazy</b></marquee>'
 }, {
     id: 2,
-    primaryPhoneNumber: '(425) 999-99-89',
+    primaryPhoneNumber: {
+        type: 0,
+        countryCode: 0,
+        number: 8989989898,
+        extension: null,
+        areaCode: null,
+        isPrimary: false,
+        formattedNumber: null
+    },
+    profilePicture:{
+        id: 8649,
+        primaryVersion: {
+          fileName: "22770061-1849016341.jpg",
+          directoryName: "ad-products.cdn.memberphotos/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.memberphotos/22770061-1849016341.jpg",
+          width: 95,
+          height: 40,
+          alternateText: null
+          },
+        originalVersion: {
+          fileName: "22770061-2087991557.jpg",
+          directoryName: "ad-products.cdn.originalmemberphotos/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.originalmemberphotos/22770061-2087991557.jpg",
+          width: 375,
+          height: 158,
+          alternateText: null
+          },
+        thumbnailVersion: {
+          fileName: "22770061-233085406.jpg",
+          directoryName: "ad-products.cdn.memberthumbs/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.memberthumbs/22770061-233085406.jpg",
+          width: 50,
+          height: 21,
+          alternateText: null
+          },
+        size75Version: {
+          fileName: "22770061-1542256979.jpg",
+          directoryName: "ad-products.cdn.member75x75/",
+          url: "//sac-dev-fs2.dev.ingenio.com/ad-products.cdn.member75x75/22770061-1542256979.jpg",
+          width: 75,
+          height: 31,
+          alternateText: null
+          },
+        isAssociatedWithService: true
+    },
     categoryId: 642,
     advisorName: 'Muthu Vynogradenko',
     title: '',
-
     //todo:
     // specializedSituations: [{id:1},{id:2}],   //array of numbers
     // skills: [{id:1},{id:6},{id:12}],  //array of numbers
